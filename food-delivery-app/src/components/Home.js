8d988877565a33b65c960cd91d5fde3b2fb16cc6
YashwanthKore <yashwanthkore97@gmail.com>
2020-06-13 13:41:23 +0530

    Created Navbar and Footer

---
+++ food-delivery-app/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+let card = {height:"200px",width:"20%",padding:"2%"}
+function Home(){
+    return(
+        <div> 
+            restaurents    
+        </div>
+    )
+}
+export default Home
